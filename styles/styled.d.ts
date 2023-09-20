2ee619bc3df1e7ef987c46171ad52e35135ca0bc
alexalvim <alex.alvim19@gmail.com>
2023-09-20 18:26:21 -0300

    Created base theme and installed EsLint

---
+++ styles/styled.d.ts
@@ -0,0 +1,40 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string
+
+    colors: {
+      lightestColor: string
+      mainColor: string
+      darkestColor: string
+      lightGray: string
+      darkGray: string
+      blue: string
+      dangerRed: string
+    }
+
+    spaces: {
+      50: string
+      100: string
+      200: string
+      300: string
+      400: string
+      500: string
+      600: string
+      700: string
+      800: string
+      900: string
+    }
+
+    typo: {
+      100: string
+      200: string
+      300: string
+      400: string
+      500: string
+      600: string
+      700: string
+    }
+  }
+}
---
+++ styles/themes/light.ts
@@ -0,0 +1,36 @@
+export const light = {
+  title: 'light',
+
+  colors: {
+    lightestColor: '#FFFFFF',
+    mainColor: '#E40F0F',
+    darkestColor: '#000000',
+    lightGray: '#D5D5D5',
+    darkGray: '#747474',
+    blue: '#0FA4E4',
+    dangerRed: '#DF8686',
+  },
+
+  spaces: {
+    50: '0.25rem',
+    100: '0.5rem',
+    200: '1rem',
+    300: '1.5rem',
+    400: '2rem',
+    500: '2.5rem',
+    600: '3rem',
+    700: '3.5rem',
+    800: '4rem',
+    900: '4.5rem',
+  },
+
+  typo: {
+    100: '0.75rem',
+    200: '0.875rem',
+    300: '1rem',
+    400: '1.125rem',
+    500: '1.25rem',
+    600: '1.5rem',
+    700: '2rem',
+  },
+}
