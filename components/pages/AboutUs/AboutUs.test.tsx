c02df0cbae0782d515b2a4daaa294ce0fcda221d
alexalvim <alex.alvim19@gmail.com>
2023-09-25 21:21:26 -0300

    Created AboutUs tests

---
+++ components/pages/AboutUs/AboutUs.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+
+import { ThemeProvider } from 'styled-components'
+import { AboutUs } from '.'
+import { light } from '../../../styles/themes/light'
+
+describe('PageSummary', () => {
+  it('should render component correctly', () => {
+    render(
+      <ThemeProvider theme={light}>
+        <AboutUs />
+      </ThemeProvider>,
+    )
+    const mainTitle = screen.getByText('O Centro Pokémon')
+    const howItWorksTitle = screen.getByText(
+      'Como funciona a cura de um pokémon?',
+    )
+    const traditionTitle = screen.getByText('Uma tradição de mais de 20 anos')
+    const bestToYourPokemonTitle = screen.getByText('O melhor para seu pokémon')
+    const highTecnologiTitle = screen.getByText('Alta Tecnologia')
+
+    expect(mainTitle).toBeInTheDocument()
+    expect(howItWorksTitle).toBeInTheDocument()
+    expect(traditionTitle).toBeInTheDocument()
+    expect(bestToYourPokemonTitle).toBeInTheDocument()
+    expect(highTecnologiTitle).toBeInTheDocument()
+  })
+})
